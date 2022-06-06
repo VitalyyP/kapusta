9074ea2fcfc27202e1c5c8b9e1a6d1041c91f772
Vitalii Pikozh <wisart@gmail.com>
2022-06-06 23:53:44 +0300

    add optimisation

--- src/pages/Statistics/index.js
+++ src/pages/Statistics/index.js
@@ -1,18 +1,39 @@
-import { useState } from "react";
-import Modal from "../../components/Modal";
+import { useState, memo, useMemo } from "react";
 
-const Statistics = () => {
-  const [openModal, setOpenModal] = useState(false);
+const list = new Array(20).fill(0).map(() => `Item - ${Math.random()}`);
+
+const List = ({ list }) => {
+  const [filter, setFilter] = useState("");
+  const filteredList = list.filter((item) => item.includes(filter), [filter]);
   return (
     <>
-      <h1>Statistics page</h1>
-      <button onClick={() => setOpenModal(true)}>Open</button>
-
-      <Modal open={openModal} onClose={() => setOpenModal(false)}>
-        <div className="123345">I'm in portal</div>
-        <button onClick={() => setOpenModal(false)}>Close</button>
-      </Modal>
+      <ul>
+        {filteredList.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+      <input type="text" onChange={(e) => setFilter(e.target.value)} />
     </>
+  );
+};
+
+const Clicker = ({ children }) => {
+  const [n, setN] = useState(0);
+
+  return (
+    <div data-count={n}>
+      {children}
+      <p>Clicked {n} times</p>
+      <button onClick={() => setN(n + 1)}>Click me</button>
+    </div>
+  );
+};
+
+const Statistics = () => {
+  return (
+    <Clicker>
+      <List list={list} />
+    </Clicker>
   );
 };
 
